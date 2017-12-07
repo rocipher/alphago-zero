efc280c3d701a0b98928792b05a93fe39dd4e601
Mihai Chiru <mihai.chiru@gmail.com>
2017-12-07 10:29:23 +0100

    Implemented multiprocess sync mechanism with pipes

--- model_parallel.py
+++ model_parallel.py
@@ -5,43 +5,37 @@ import ctypes
 import os
 from enum import Enum
 from multiprocessing import Manager, Event, Value, Array
-from multiprocessing import Queue
+from multiprocessing import Queue, Pipe
+import multiprocessing
 from queue import Empty, Full
 import uuid
 import time
+from collections import deque
 
 from hyper_params import *
 from go_board import *
 from model import *
 
 class ControlActions(Enum):
+    STOP = -1,
     SAVE = 1,
-    SAVE_COMPLETED = 2
+    SAVE_COMPLETED = 2,
+    TRAIN = 3,
+    TRAIN_COMPLETED = 4
 
 class MultiProcessModelProxy(Model):
-    def __init__(self, model_file:str=None, 
+    def __init__(self, pipe_id=None,
+                    model_file:str=None,                     
                     predict_queue:Queue=None,
                     history_queue=None,
-                    stop_event:Event=None,
-                    results:dict=None,
-                    train_model_event:Event=None,
-                    train_completed_event:Event=None,
-                    prediction_ready_event:Event=None,
-                    control_pipe=None,
-                    log_queue=None):
+                    results_pipe=None):
         super(MultiProcessModelProxy, self).__init__()
 
+        self.pipe_id = pipe_id
         self.model_file = model_file
         self.predict_queue = predict_queue
         self.history_queue = history_queue        
-        self.stop_event = stop_event
-        self.results = results
-        self.train_model_event = train_model_event
-        self.train_completed_event = train_completed_event
-        self.control_pipe = control_pipe
-        self.prediction_ready_event = prediction_ready_event
-        self.log_queue = log_queue
-        self.train_batch_index = 0
+        self.results_pipe = results_pipe
 
         logging.info("MultiProcessModelProxy init: %s", model_file)
 
@@ -49,29 +43,16 @@ class MultiProcessModelProxy(Model):
         raise NotImplementedError()
 
     def predict(self, state):
-        result_id = uuid.uuid4()
-        self.predict_queue.put((result_id, state), block=True)        
-        while True:
-            try:
-                self.prediction_ready_event.wait()
-                ret_val, ret_probs = self.results[result_id]
-                del self.results[result_id]
-                break
-            except KeyError:
-                pass
+        self.predict_queue.put((self.pipe_id, state), block=True)
+        ret_val, ret_probs = self.results_pipe.recv()
         return ret_val, ret_probs 
-        
-    def train(self):
-        self.train_completed_event.clear()
-        self.train_model_event.set()
-        self.train_completed_event.wait()            
-    
+           
     def copy(self):
         raise NotImplementedError()
 
     def save(self, filePath):
         raise NotImplementedError()
-
+        
 
 
 def init_log_queue(log_queue:Queue):
@@ -81,62 +62,68 @@ def init_log_queue(log_queue:Queue):
     _LOG_QUEUE_HANDLER = QueueHandler(log_queue)
     logger = logging.getLogger()
     logger.setLevel(LOG_LEVEL)
+    logger.handlers.clear()
     logger.addHandler(_LOG_QUEUE_HANDLER)
+    mp_logger = multiprocessing.log_to_stderr()    
+    mp_logger.setLevel(LOG_LEVEL)
 
-def _run_model_prediction(model, results:dict, buffer:list):
+def _run_model_prediction(model, buffer:list):    
     states = [state for _, state in buffer]
     values, act_probs = model.predict(states)
-    for value, act_prob, (result_id, _) in zip(values, act_probs, buffer):
-        results[result_id] = (value, act_prob)
+    results = {}
+    for value, act_prob, (client_id, _) in zip(values, act_probs, buffer):
+        results[client_id] = (value, act_prob)
+    return results
 
-def _train_model(model:MultiProcessModelProxy, history_queue, start_batch_index:int, num_train_iter:int):
-    for train_iter_idx in np.arange(start_batch_index, start_batch_index+num_train_iter):
-        logging.debug("Network train: %d", train_iter_idx)
-        batch_indices = np.random.choice(np.arange(len(history_queue)), size=BATCH_SIZE)
+def _train_model(model:MultiProcessModelProxy, history_queue, iter_index:int, num_train_iter:int):
+    model.set_seed(iter_index)
+    for train_iter_idx in np.arange(iter_index*num_train_iter, (iter_index+1)*num_train_iter):
+        logging.debug("Network train: train_batch_idx: %04d", train_iter_idx)
+        batch_indices = np.random.choice(len(history_queue), size=BATCH_SIZE)
         batch = [history_queue[indx] for indx in batch_indices]
-        model.train_on_hist_batch(batch, train_iter_idx)            
+        model.train_on_hist_batch(batch, train_iter_idx)
 
 
-def run_loop(model_proxy:MultiProcessModelProxy):    
+def run_loop(model_proxy:MultiProcessModelProxy, control_pipe:Pipe, clients:dict):    
     logging.info("run_loop: model_file: %s", model_proxy.model_file)
 
-    # this method runs on it's own thread
+    # this method runs on its own thread
     model = SimpleNNModel()
     if model_proxy.model_file is not None:
         model.load(model_proxy.model_file)
 
     local_predict_buffer = []
     should_stop = False        
+
     while not should_stop:  
-        try:
-            msg = model_proxy.predict_queue.get(block=True, timeout=1)
-            local_predict_buffer.append(msg)
+        try:    
+            msg = model_proxy.predict_queue.get(block=True, timeout=1.)
+            local_predict_buffer.append(msg)            
             while len(local_predict_buffer) < PREDICTION_QUEUE_BATCH_SIZE:
                 msg = model_proxy.predict_queue.get(block=False)
                 local_predict_buffer.append(msg)
         except Empty:
-            pass            
+            pass
 
-        if  len(local_predict_buffer) > 0:            
+        if  len(local_predict_buffer) > 0:         
             logging.debug("run prediction: %d", len(local_predict_buffer))
-            _run_model_prediction(model, model_proxy.results, local_predict_buffer)
+            results = _run_model_prediction(model, local_predict_buffer)
+            for client_id, result in results.items():
+                clients[client_id].send(result)
             local_predict_buffer = []
-            model_proxy.prediction_ready_event.set()
-            model_proxy.prediction_ready_event.clear()
-                    
-        if model_proxy.control_pipe and model_proxy.control_pipe.poll(0):
-            act, args = model_proxy.control_pipe.recv()
+
+        if control_pipe.poll(0):
+            act, args = control_pipe.recv()
+            logging.info("Control pipe received: act=%s, args=%s", act, args)
             if act == ControlActions.SAVE:
                 model.save(args)
-                model_proxy.control_pipe.send(ControlActions.SAVE_COMPLETED)
+                control_pipe.send((ControlActions.SAVE_COMPLETED, None))
+            elif act == ControlActions.TRAIN:
+                iter_index = args
+                _train_model(model, model_proxy.history_queue, iter_index, NUM_TRAIN_LOOP_ITER)
+                control_pipe.send((ControlActions.TRAIN_COMPLETED, None))
+            elif act == ControlActions.STOP:
+                should_stop = True
             else:
                 raise ValueError("Unknown action %d" % (act))
-
-        should_train = model_proxy.train_model_event.wait(0)
-        if should_train:
-            _train_model(model, model_proxy.history_queue, model_proxy.train_batch_index, NUM_TRAIN_LOOP_ITER)
-            model_proxy.train_batch_index += NUM_TRAIN_LOOP_ITER
-            model_proxy.train_model_event.clear()
-            model_proxy.train_completed_event.set()
-        
-        should_stop = model_proxy.stop_event.wait(0)
+    #end while
--- train_parallel.py
+++ train_parallel.py
@@ -5,81 +5,174 @@ from logging.handlers import QueueHandler, QueueListener
 import numpy as np
 import datetime
 import signal
-from itertools import repeat
+import ctypes
+from itertools import repeat, cycle, product
 import traceback
 from multiprocessing.pool import Pool
 from multiprocessing import Manager, Queue
 from multiprocessing import Process, Pipe, Event
+import multiprocessing
 
 import model_parallel
 from model_parallel import ControlActions, MultiProcessModelProxy
 import hyper_params as hp
 import game
 
-def pool_worker_init():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+from collections import namedtuple
+from namedlist import namedlist
 
-def nn_worker(log_queue:Queue, model_proxy:MultiProcessModelProxy):
-    model_parallel.init_log_queue(log_queue)
-    model_parallel.run_loop(model_proxy)
+ModelWorker = namedtuple("ModelWorker", ["process", "model_proxy", "control_pipe", "client_pipes"])
 
-def self_play_worker(game_index:int, model_proxy:MultiProcessModelProxy):
-    model_parallel.init_log_queue(model_proxy.log_queue)
-    result = game.self_play(game_index, model_proxy, model_proxy.history_queue, 
-                noise_alpha=hp.DIRICHLET_ALPHA, temperatures=hp.TEMPERATURES)
-    return result
+
+class WorkersState:
+    def __init__(self, model_proxies:list=[], manager:Manager=None):
+        self.model_proxies = model_proxies
+        self.available_queue = manager.Queue()
+        for worker_proxy_id in range(len(model_proxies)):
+            self.available_queue.put(worker_proxy_id)
     
-def multi_player_worker(game_index:int, model_a:MultiProcessModelProxy, model_b:MultiProcessModelProxy):
-    model_parallel.init_log_queue(model_a.log_queue)
-    result = game.two_player_play(game_index, model_a, model_b)
+    def aquire_available_model_proxy(self):
+        worker_proxy_id = self.available_queue.get(block=True)
+        model_proxy = self.model_proxies[worker_proxy_id]
+        return worker_proxy_id, model_proxy
+
+    def release_model_proxy(self, worker_proxy_id):
+        self.available_queue.put(worker_proxy_id)        
+
+
+def pool_worker_init(log_queue):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)    
+    model_parallel.init_log_queue(log_queue)    
+
+
+def self_play_worker(iter_index: int, game_index:int, history_queue:Queue, worker_state:WorkersState):
+    np.random.seed(int(hp.SEED * 1e6 + iter_index * 1e4 + game_index))
+        
+    worker_proxy_id, model_proxy = worker_state.aquire_available_model_proxy()
+    logging.info("Self-play game %d using model proxy: %d", game_index, worker_proxy_id)
+    result = game.self_play(game_index, model_proxy, history_queue, 
+                    noise_alpha=hp.DIRICHLET_ALPHA, temperatures=hp.TEMPERATURES)
+    worker_state.release_model_proxy(worker_proxy_id)
     return result
 
-def spawn_nn_process(model_file:str, manager:Manager, history_queue, log_queue: Queue):
+    
+def multi_player_worker(iter_index: int, game_index:int, model_a_state:WorkersState, model_b_state:WorkersState):
+    np.random.seed(int(hp.SEED * 1e6 + iter_index * 1e4 + game_index))
+
+    model_a_id, model_a = model_a_state.aquire_available_model_proxy()
+    model_b_id, model_b = model_b_state.aquire_available_model_proxy()
+    result = game.two_player_play(game_index, model_a, model_b)
+    model_a_state.release_model_proxy(model_a_id)
+    model_b_state.release_model_proxy(model_b_id)
+    return result
+
+def nn_worker(log_queue:Queue, model_proxy:MultiProcessModelProxy, control_pipe:Pipe, clients:dict):
+    model_parallel.init_log_queue(log_queue)
+    np.random.seed(hp.SEED)
+    model_parallel.run_loop(model_proxy, control_pipe, clients)
+
+def spawn_nn_process(model_file:str, manager:Manager, 
+                     history_queue, log_queue: Queue,
+                     num_clients:int):
     parent_ctrl_pipe, child_ctrl_pipe = Pipe()    
-    pl_model = MultiProcessModelProxy(model_file=model_file,
-                                        predict_queue=manager.Queue(maxsize=hp.PREDICTION_QUEUE_BATCH_SIZE),
-                                        history_queue=history_queue,
-                                        stop_event=manager.Event(),
-                                        results=manager.dict(),
-                                        train_model_event=manager.Event(),
-                                        train_completed_event=manager.Event(),
-                                        control_pipe=parent_ctrl_pipe,                                        
-                                        prediction_ready_event=manager.Event(),
-                                        log_queue=log_queue)
-    nn_process = Process(target=nn_worker, args=(log_queue, pl_model))
+    send_pipe_ends = {}
+    recv_pipe_ends = {}
+    for client_id in range(num_clients):
+        recv_pipe_ends[client_id], send_pipe_ends[client_id] = Pipe(duplex=False)
+    pl_model = MultiProcessModelProxy(pipe_id=None,  
+                                    model_file=model_file,
+                                    predict_queue=manager.Queue(maxsize=hp.PREDICTION_QUEUE_BATCH_SIZE),
+                                    history_queue=history_queue)
+    nn_process = Process(target=nn_worker, args=(log_queue, pl_model, parent_ctrl_pipe, send_pipe_ends))
     nn_process.daemon = True
     nn_process.start()
-    return nn_process, pl_model, child_ctrl_pipe
+    return ModelWorker(nn_process, pl_model, child_ctrl_pipe, recv_pipe_ends)
 
 
-def train_loop(manager:Manager, log_queue:Queue):    
-    pool = Pool(processes=hp.NUM_WORKERS, initializer=pool_worker_init)
+def spawn_workers(num_workers, *args):
+    workers = []
+    for pred_model_ix in range(num_workers):
+        model_worker = spawn_nn_process(*args)
+        workers.append(model_worker)
+    return workers
+
+def stop_and_join_workers(*workers):
+    try:
+        for worker in workers:
+            worker.control_pipe.send((ControlActions.STOP, None))
+        for worker in workers:
+            worker.process.join(1.)
+    finally:
+        terminate_workers(*workers)
+
+def terminate_workers(*workers):
+    for worker in workers:
+        worker.process.terminate()
+
+def save_worker_model(model_worker, model_file):
+    model_worker.control_pipe.send((ControlActions.SAVE, model_file))
+    if model_worker.control_pipe.poll(30.):
+        save_result, _ = model_worker.control_pipe.recv()
+        if save_result != ControlActions.SAVE_COMPLETED:
+            raise Exception("Save model failed: %s" % model_file)
+    else:
+        logging.error("Model save action didn't receive an answer! model_file: %s", model_file)
+
+
+def spawn_predict_proxies(num_workers:int, num_proxies_per_worker:int, model_file:str, 
+                        manager:Manager, history_queue:Queue, log_queue:Queue):
+    workers = spawn_workers(num_workers, model_file, manager, history_queue, log_queue, num_proxies_per_worker)
+    wstate = WorkersState(model_proxies=[MultiProcessModelProxy(pipe_id=pipe_id,
+                                                                predict_queue=w.model_proxy.predict_queue,
+                                                                results_pipe=pipe)
+                                                    for w in workers
+                                                    for pipe_id, pipe in w.client_pipes.items()],
+                            manager=manager)
+    return workers, wstate
+    
+
+def train_loop(manager:Manager, log_queue:Queue):
+    pool = Pool(processes=hp.NUM_POOL_WORKERS, 
+                initializer=pool_worker_init,   
+                initargs=(log_queue,),
+                maxtasksperchild=hp.MAX_GAMES_PER_POOL_WORKER)
 
     history_queue = manager.list()    
 
-    best_model_file = hp.MODEL_FILE
-    nn_train_proc_best, pl_model_best, ctrl_pipe_best = spawn_nn_process(
-                best_model_file, manager, history_queue, log_queue)
+    self_play_model_file = hp.SELF_PLAY_MODEL_FILE
+    trained_model_file = hp.TRAIN_MODEL_FILE
 
-    trained_model_file = hp.MODEL_FILE
-    nn_train_proc_train, pl_model_train, ctrl_pipe_train = spawn_nn_process(
-                trained_model_file, manager, history_queue, log_queue)    
+    num_clients_per_predict_worker = hp.NUM_POOL_WORKERS // hp.NUM_PREDICT_WORKERS
+
+    self_play_predict_workers, self_play_model_wstate = spawn_predict_proxies(hp.NUM_PREDICT_WORKERS, 
+                        num_clients_per_predict_worker, self_play_model_file, manager, history_queue, log_queue)
+
+    train_workers, train_model_wstate = spawn_predict_proxies(1, hp.NUM_POOL_WORKERS, self_play_model_file, 
+                                            manager, history_queue, log_queue)
+    assert len(train_workers) == 1
+    train_worker = train_workers[0]
     
     try:
-        for iter_index in range(hp.NUM_ITER):
+        for iter_index in range(hp.START_ITER, hp.START_ITER+hp.NUM_ITER):
             # self-play
-            logging.info("Iter %d: Starting self-play", iter_index)
-            self_play_results = pool.starmap(self_play_worker, zip(range(hp.NUM_GAMES), repeat(pl_model_best)))
+            logging.info("Iter %d: Starting self-play", iter_index)            
+            self_play_results = pool.starmap(self_play_worker, 
+                                    zip(repeat(iter_index), range(hp.NUM_GAMES), 
+                                    repeat(history_queue), repeat(self_play_model_wstate)))
             logging.info("Iter %d, self-play results: %s", iter_index, self_play_results)
 
             # train NN
             logging.info("Iter %d: Starting network train", iter_index)
-            pl_model_train.train()
+            train_worker.control_pipe.send((ControlActions.TRAIN, None))
+            act, result = train_worker.control_pipe.recv()
+            assert act == ControlActions.TRAIN_COMPLETED
             logging.info("Iter %d: Ended network train", iter_index)
 
             # eval
             logging.info("Iter %d: Starting evaluation", iter_index)
-            eval_results = pool.starmap(multi_player_worker, zip(range(hp.NUM_EVAL_GAMES), repeat(pl_model_train), repeat(pl_model_best)))
+            eval_results = pool.starmap(multi_player_worker, zip(
+                                    repeat(iter_index), range(hp.NUM_EVAL_GAMES),
+                                    repeat(train_model_wstate), repeat(self_play_model_wstate)))
             logging.info("Iter %d: Evaluation end: results: %s", iter_index, eval_results)
             outcomes = np.array([outcome for _,outcome in eval_results])
             trained_model_win_ratio = np.sum(outcomes == hp.OUTCOME_WIN_PLAYER_1)/len(outcomes)
@@ -87,21 +180,18 @@ def train_loop(manager:Manager, log_queue:Queue):
             logging.info("Iter %d evaluation: trained_model_win percent : %.2f%%", iter_index, trained_model_win_ratio*100)
 
             if trained_model_win_ratio > hp.MIN_MODEL_REPLACEMENT_WIN_RATIO:
-                pl_model_best.stop_event.set()
-                nn_train_proc_best.join(1.)
-                nn_train_proc_best.terminate()
+                stop_and_join_workers(*self_play_predict_workers)
+                del self_play_model_wstate                
+                del self_play_predict_workers
+                                
+                self_play_model_file = "%s/model-best-%s-%d-%.0f.h5" % (hp.OUTPUT_DIR, log_id, iter_index, trained_model_win_ratio*100)
+                save_worker_model(train_worker, self_play_model_file)                
                 
-                best_model_file = "%s/model-%s-%d-%.0f.h5" % (hp.OUTPUT_DIR, log_id, iter_index, trained_model_win_ratio*100)
-                ctrl_pipe_train.send((ControlActions.SAVE, best_model_file))
-                if ctrl_pipe_train.poll(30.):
-                    save_result = ctrl_pipe_train.recv()
-                    if save_result != ControlActions.SAVE_COMPLETED:
-                        raise Exception("Save model failed.")
-                else:
-                    logging.error("Model save action didn't receive an answer! iter_index: %d, new_model_file: %s", iter_index, best_model_file)
+                self_play_predict_workers, self_play_model_wstate = spawn_predict_proxies(hp.NUM_PREDICT_WORKERS, 
+                        num_clients_per_predict_worker, self_play_model_file, manager, history_queue, log_queue)
 
-                nn_train_proc_best, pl_model_best, ctrl_pipe_best = spawn_nn_process(
-                            best_model_file, manager, history_queue, log_queue)
+            trained_model_file = "%s/model-train-%s-%d-%.0f.h5" % (hp.OUTPUT_DIR, log_id, iter_index, trained_model_win_ratio*100)
+            save_worker_model(train_worker, trained_model_file)
 
             gc.collect()
         # end iter loop
@@ -112,16 +202,10 @@ def train_loop(manager:Manager, log_queue:Queue):
             logging.error("Error: %s", e)
         pool.terminate()
         pool.join()
-        nn_train_proc_train.terminate()
-        nn_train_proc_best.terminate()
+        terminate_workers(train_worker, *self_play_predict_workers)
         raise e
     else:
-        pl_model_train.stop_event.set()
-        pl_model_best.stop_event.set()
-        
-        nn_train_proc_train.join()
-        nn_train_proc_best.join()
-
+        stop_and_join_workers(train_worker, *self_play_predict_workers)        
         logging.info('Done successfully.')
     finally:
         pool.close()        
@@ -141,8 +225,12 @@ def logger_init(manager: Manager):
 
     logger = logging.getLogger()
     logger.setLevel(hp.LOG_LEVEL)
+    logger.handlers.clear()
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
+
+    mp_logger = multiprocessing.log_to_stderr()
+    mp_logger.setLevel(hp.LOG_LEVEL)
 
     return queue_listener, log_queue
 
